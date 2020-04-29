5297d46d524516a4c03eb7438c349ab04543751a
Hendriktio Freizello <hendriktio.freizello@gmail.com>
2020-04-29 23:49:55 +0700

    Mongodb Atlas connected

--- app.js
+++ app.js
@@ -2,30 +2,37 @@ const express = require("express"),
       dotenv = require('dotenv'), 
       bodyParser = require('body-parser'),
       morgan = require("morgan"),
+      mongoose = require("mongoose")
       webRoutes = require('./routes/web/index'),
       apiRoutes = require('./routes/api/index');
       
 dotenv.config()
 const port = (process.env.PORT || 8080)
-
 const app = express()
+
+// database
+mongoose.connect(process.env.MONGODB_URL, { 
+  useNewUrlParser: true,  
+  useUnifiedTopology: true 
+  }
+).then(() => console.log('DB connected'))
+// db conn error
+mongoose.connection.on("error", err => {
+  console.log(`DB connection error : ${err.message}`)
+})
 
 // middleware
 app.use(morgan("dev"))
+
+// request body handler
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended : true}))
 // parse application/json
 app.use(bodyParser.json())
 
-// web routes
-// app.get('/', webRoute)
-
-// api routes
-// app.get('/api', apiRoute)
-// app.post('/api', apiRoute)
+// routes
 app.use("/api", apiRoutes)
 app.use("/", webRoutes)
-
 
 // start
 app.listen(port, () => console.log(`app listening at http://localhost:${port}`))
